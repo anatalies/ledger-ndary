8f12db10bfe8aa605ac41c33cf51814c166515ec
aromi ben <158552866+benardaromi@users.noreply.github.com>
2024-12-12 21:30:42 +0000

    Clerk auth

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'https://bug-free-space-doodle-7vvgp6pgx5q63p4-3000.app.github.dev/',
+        'localhost:3000'
+      ]
+    }
+  }
 };
 
 export default nextConfig;
